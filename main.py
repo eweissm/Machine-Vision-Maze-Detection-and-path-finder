abe4adb070fc82befb8fc2421aff9b98e4449f18
Ericw <eweissm1@asu.edu>
2022-12-04 12:53:36 -0700

    new commit

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ lower_bound2 = np.array([90, 50, 70])
 upper_bound2 = np.array([128, 255, 255])
 
 
+
 while(True):
     retu, frame = video_0.read()
     i=0
@@ -46,10 +47,21 @@ while(True):
     contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     contours2, hierarchy2 = cv2.findContours(mask2.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    C = np.empty([len(contours2), 2], 'i')
     #output = cv2.bitwise_and(frame,segmented_img, mask =mq)
     # Draw contour on original image
     output = cv2.drawContours(frame, contours, -1, (0, 0, 255), 3)
-    output = cv2.drawContours(frame, contours2, -1, (0, 255, 0), 3)
+    output = cv2.drawContours(frame, contours2,-1, (0, 255, 0), 3)
+    #print(np.shape(contours))
+
+    print(len(contours2))
+    if len(contours2)>0:
+       for i in range(len(contours2)):
+            M = cv2.moments(contours2[i])
+            C[i,0]= int(M['m10'] / M['m00']) #cx
+            C[i,1] = int(M['m01'] / M['m00'])#cy
+            output[C[i,1]-2:C[i,1]+2,C[i,0]-2:C[i,0]+2] = [255, 255, 255]
+
     cv2.imshow('frame',output)
 
 
