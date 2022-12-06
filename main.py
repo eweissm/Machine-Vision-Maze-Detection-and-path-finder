a54e5213d812505f33fc79191dbdc44131dd7592
Ericw <eweissm1@asu.edu>
2022-12-05 18:57:44 -0700

    new commit

--- main.py
+++ main.py
@@ -15,62 +15,61 @@ color_dict_HSV = {'black': [[180, 255, 30], [0, 0, 0]],
 
 video_0 = cv2.VideoCapture(0)
 
-lower_bound = np.array([36, 50, 70])
-upper_bound = np.array([89, 255, 255])
+lower_bound = np.array([150, 150,120])
+upper_bound = np.array([170, 255, 255])
 
 lower_bound2 = np.array([90, 50, 70])
 upper_bound2 = np.array([128, 255, 255])
 
-
+d=0
 
 while(True):
     retu, frame = video_0.read()
     i=0
     j=0
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_bound, upper_bound)
 
+    mask = cv2.inRange(hsv, lower_bound, upper_bound)
     mask2 = cv2.inRange(hsv, lower_bound2, upper_bound2)
+
     # define kernel size
     kernel = np.ones((7, 7), np.uint8)
+
     # Remove unnecessary noise from mask
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-
     mask2 = cv2.morphologyEx(mask2, cv2.MORPH_CLOSE, kernel)
     mask2 = cv2.morphologyEx(mask2, cv2.MORPH_OPEN, kernel)
 
     color1 = cv2.bitwise_and(frame, frame, mask=mask)
-
     color2 = cv2.bitwise_and(frame, frame, mask=mask2)
 
     contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
     contours2, hierarchy2 = cv2.findContours(mask2.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    C = np.empty([len(contours2), 2], 'i')
-    #output = cv2.bitwise_and(frame,segmented_img, mask =mq)
+
+    C = np.empty([len(contours), 2], 'i')
+
     # Draw contour on original image
     output = cv2.drawContours(frame, contours, -1, (0, 0, 255), 3)
     output = cv2.drawContours(frame, contours2,-1, (0, 255, 0), 3)
-    #print(np.shape(contours))
 
-    #print(np.shape(frame))
-    if len(contours2)>0:
-       for i in range(len(contours2)):
-            M = cv2.moments(contours2[i])
+    print(len(contours))
+
+    if len(contours)>0:
+       for i in range(len(contours)):
+            M = cv2.moments(contours[i])
             C[i,0]= int(M['m10'] / M['m00']) #cx
             C[i,1] = int(M['m01'] / M['m00'])#cy
             output[C[i,1]-2:C[i,1]+2,C[i,0]-2:C[i,0]+2] = [255, 255, 255]
             output = cv2.putText(output, str(i),(C[i,0],C[i,1]),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 0),2,cv2.LINE_AA)
 
 
+    if len(contours) == 4:
+        destpts = np.float32([[480-d, 0], [480-d, 640-d], [0, 0], [0, 640-d]])
+        resmatrix = cv2.getPerspectiveTransform(np.float32(C), destpts)
+        frame = cv2.warpPerspective(frame, resmatrix, (480, 640))
 
-    if len(contours2) == 4:
-        destpts = np.float32([[100, 100], [540, 100], [100, 380], [540, 380]])
-        resmatrix = cv2.getPerspectiveTransform(C, destpts)
-        output = cv2.warpPerspective(output, resmatrix, (480, 640))
-
-    cv2.imshow('frame',output)
+    cv2.imshow('frame',frame)
 
 
     if cv2.waitKey(1) & 0xFF==ord('a'):
