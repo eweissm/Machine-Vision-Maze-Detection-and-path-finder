0600965195abf9299c73970fd14738ddabe0cf9c
Ericw <eweissm1@asu.edu>
2022-12-13 00:17:17 -0700

    new commit

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ from pathfinding.finder.a_star import AStarFinder
 #this function performs a pathfinding algorithm (A*) given a maze, a starting location and an end location
 #the maze is a matrix where zeros are the wall of the maze and numbers are locations where travel is allowed. The higher the number the higher the cost
 def FindPath(maze, startPoint, endPoint):
-    #print(np.shape(endPoint))
+
     grid = Grid(np.size(maze,1),np.size(maze,0) ,matrix=maze, inverse=True)
     start = grid.node(startPoint[0], startPoint[1])
     end = grid.node(endPoint[0], endPoint[1])
     finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
     path, runs = finder.find_path(start, end, grid)
-    #print('operations:', runs, 'path length:', len(path))
-    #print(grid.grid_str(path=path, start=start, end=end))
+
     return path
 
 #connect camera
@@ -29,6 +28,9 @@ upper_bound = np.array([170, 255, 255])
 
 lower_bound2 = np.array([36, 50, 70])
 upper_bound2 = np.array([89, 255, 255])
+
+lower_bound3 = np.array([25, 50, 70])
+upper_bound3 = np.array([35, 255, 255])
 
 meshSize =10
 
@@ -50,6 +52,7 @@ while(True):
 #find color masks
     mask = cv2.inRange(hsv, lower_bound, upper_bound)
     mask2 = cv2.inRange(hsv, lower_bound2, upper_bound2)
+    mask3 = cv2.inRange(hsv, lower_bound3, upper_bound3)
 
     # define kernel size
     kernel = np.ones((7, 7), np.uint8)
@@ -59,41 +62,29 @@ while(True):
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     mask2 = cv2.morphologyEx(mask2, cv2.MORPH_CLOSE, kernel)
     mask2 = cv2.morphologyEx(mask2, cv2.MORPH_OPEN, kernel)
+    mask3 = cv2.morphologyEx(mask3, cv2.MORPH_CLOSE, kernel)
+    mask3 = cv2.morphologyEx(mask3, cv2.MORPH_OPEN, kernel)
 
     color1 = cv2.bitwise_and(frame, frame, mask=mask)
     color2 = cv2.bitwise_and(frame, frame, mask=mask2)
+    color3 = cv2.bitwise_and(frame, frame, mask=mask3)
 
 #finds contours from colors
     contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours2, hierarchy2 = cv2.findContours(mask2.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours3, hierarchy3 = cv2.findContours(mask3.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 #array of center points of contours
     C = np.empty([len(contours), 2], 'i')
     C2 = np.empty([len(contours2), 2], 'i')
+    C3 = np.empty([len(contours3), 2], 'i')
+
     #fixed = np.empty([len(contours), 2], 'i')
 
     # Draw contour on original image
     output = cv2.drawContours(frame, contours, -1, (0, 0, 255), 5)
     output = cv2.drawContours(frame, contours2,-1, (0, 255, 0), 5)
-
-#finds centerpoint of colored dots... adds to array and adds dot to image
-    if len(contours)>0:
-       for i in range(len(contours)):
-            M = cv2.moments(contours[i])
-            C[i,0]= int(M['m10'] / M['m00']) #cx
-            C[i,1] = int(M['m01'] / M['m00'])#cy
-            output[C[i,1]-2:C[i,1]+2,C[i,0]-2:C[i,0]+2] = [255, 255, 255]
-            output = cv2.putText(output, str(i),(C[i,0],C[i,1]),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 0),2,cv2.LINE_AA)
-
-
-#converts maze wall into a pixelated version with a given mesh size
-#This will reduce the computer power needed to do pathfinding and it will make it so glitchy contours dont get missed
-       for a in range(np.size(meshedMazeWalls,0)):
-            for b in range(np.size(meshedMazeWalls, 1)):
-
-                if np.any(color1[a*meshSize:a*meshSize+meshSize, b*meshSize:b*meshSize+meshSize]):
-                    meshedMazeWalls[a][b]=1
-                    mazeFrame[a][b] = [0, 0, 0]
+    output = cv2.drawContours(frame, contours3,-1, (255, 0, 0), 5)
 
 #finds centerpoint of colored dots... adds to array and adds dot to image
     if len(contours2) > 0:
@@ -107,6 +98,32 @@ while(True):
     if len(contours2) > 1:
         cv2.line(output, (C2[0,0],C2[0,1]),(C2[1,0],C2[1,1]), (255, 0, 0), 2 )
 
+    if len(contours3) > 0:
+        for i in range(len(contours3)):
+            M = cv2.moments(contours3[i])
+            C3[i, 0] = int(M['m10'] / M['m00'])  # cx
+            C3[i, 1] = int(M['m01'] / M['m00'])  # cy
+            output[C3[i, 1] - 2:C3[i, 1] + 2, C3[i, 0] - 2:C3[i, 0] + 2] = [255, 255, 255]
+            output = cv2.putText(output, str(i), (C3[i, 0], C3[i, 1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (245, 244, 66),2, cv2.LINE_AA)
+
+# transform the image to rectangularize it---------------------------------------------------------------------------
+    if len(contours3) == 4:
+        destpts = np.float32([[0, cameraYDim], [cameraXDim,cameraYDim], [0, 0], [cameraXDim, 0]])
+        resmatrix = cv2.getPerspectiveTransform(np.float32(C3), destpts)
+        frame = cv2.warpPerspective(frame, resmatrix, (cameraXDim, cameraYDim))
+        #color1=cv2.warpPerspective(color1, resmatrix, (cameraXDim, cameraYDim))
+
+#finds centerpoint of colored dots... adds to array and adds dot to image
+    if len(contours)>0:
+#converts maze wall into a pixelated version with a given mesh size
+#This will reduce the computer power needed to do pathfinding and it will make it so glitchy contours dont get missed
+       for a in range(np.size(meshedMazeWalls,0)):
+            for b in range(np.size(meshedMazeWalls, 1)):
+
+                if np.any(color1[a*meshSize:a*meshSize+meshSize, b*meshSize:b*meshSize+meshSize]):
+                    meshedMazeWalls[a][b]=1
+                    mazeFrame[a][b] = [0, 0, 0]
+## Path finding stuff------------------------------------------------------------------------
     if len(contours2)>=2:
         start = np.array([int(C2[0,0]/meshSize), int(C2[0,1]/meshSize)], 'i')
         end = np.array([int(C2[1,0]/meshSize), int(C2[1,1]/meshSize)], 'i')
@@ -126,13 +143,7 @@ while(True):
        #for j in [1, 2, 3]:
            #if C[0,0] > C[j,0] & C[0,1] < C[j,1]
 
-# transform the image to rectangularize it
-    if len(contours) == 4:
-        destpts = np.float32([[0, cameraYDim], [cameraXDim,cameraYDim], [0, 0], [cameraXDim, 0]])
-        resmatrix = cv2.getPerspectiveTransform(np.float32(C), destpts)
-        frame = cv2.warpPerspective(frame, resmatrix, (cameraXDim, cameraYDim))
-
-#output images
+#output images----------------------------------------------------------------------------------------
     cv2.imshow('frame',frame)
     cv2.imshow('frame2', output)
     cv2.imshow('frame3', cv2.resize(mazeFrame,[cameraXDim,cameraYDim]))
